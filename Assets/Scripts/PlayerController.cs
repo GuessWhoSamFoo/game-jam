31c18dcb773eedb1ee2fce1019a8189e29646485
Sam Foo <sfoohei@gmail.com>
2020-10-24 16:36:43 -0700

    Initial commit
    Signed-off-by: Sam Foo <sfoohei@gmail.com>

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 800.0f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown ("space") && rb.transform.position.y <= 10f) {
+            Vector3 jump = new Vector3 (0.0f, 200.0f, 0.0f);
+            rb.AddForce (jump);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+## README
+
+Directory Structure
+
+```
+```
+
+`.gitignore` - See (https://github.com/github/gitignore/blob/master/Unity.gitignore)[https://github.com/github/gitignore/blob/master/Unity.gitignore]
